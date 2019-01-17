5723358cb50205c9580a56808e7d41f04173ce68
Manuel Jesus Extremera Extremera <manuel.extremera@scmspain.com>
2019-01-17 10:27:22 +0100

    Primeros 2 ejercicios

--- Modulo2/Program.cs
+++ Modulo2/Program.cs
@@ -11,6 +11,22 @@ namespace Modulo2
         static void Main(string[] args)
         {
             #region Ejercicio 1
+            //Uso de variables. En C# las variables son case sensitive. 
+            string mensaje = "Mensaje minus", Mensaje = "Mensaje 1a mayus", MENSAJE = "Mensaje todo mayus";
+            Console.WriteLine(mensaje);
+            Console.WriteLine(Mensaje);
+            Console.WriteLine(MENSAJE);
+            Console.ReadLine();
+            #endregion
+
+            #region Ejercicio 2
+            //Uso de variables. Los nombres de las variables deben ser únicas (aunque difieran solo en tipo).
+            //string variableTestA;
+            //int variableTestA;
+            #endregion
+
+            #region Ejercicio 3
+
             #endregion
         }
     }
